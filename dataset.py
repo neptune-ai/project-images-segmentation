5856446b35ba4cca7ff70e726ea3ef580fc98a29
kshitij12345 <kshitijkalambarkar@gmail.com>
2022-06-25 16:18:40 +0000

    update validation cases count

--- dataset.py
+++ dataset.py
@@ -23,7 +23,7 @@ class BrainSegmentationDataset(Dataset):
         image_size=256,
         subset="train",
         random_sampling=True,
-        validation_cases=5,
+        validation_cases=10,
         seed=42,
     ):
         assert subset in ["all", "train", "validation"]
