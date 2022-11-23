a5ebb3185b7a93b620e054d33fd76c4a755ff7fc
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2022-11-23 16:38:08 +0100

    Replaced `get_project` with `init_project`

--- ci_evaluate.py
+++ ci_evaluate.py
@@ -7,7 +7,7 @@ from dataset import BrainSegmentationDataset
 from model_utils import DiceLoss, UNet
 
 # (neptune) fetch project
-project = neptune.get_project(name="common/project-images-segmentation")
+project = neptune.init_project(name="common/project-images-segmentation")
 
 # (neptune) find best run
 best_run_df = project.fetch_runs_table(tag="best").to_pandas()
